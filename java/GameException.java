d262114c8d46879e2cbc75d1d5c6f0f8b1af0962
avr914 <arvindvjkumar@gmail.com>
2016-09-01 14:47:57 -0500

    cleaned up repo

--- java/GameException.java
+++
@@ -1,5 +0,0 @@
-/**
- * The base Exception class thrown when there's a game error and
- * somebody's disqualified.
- **/
-abstract public class GameException extends Exception {}
--- java/TimeoutException.java
+++
@@ -1,9 +0,0 @@
-/**
- * Returned in {@link OthelloResult} when a player fails to return
- * from {@link OthelloPlayer#doMove} within the timeout.
- **/
-public class TimeoutException extends GameException {
-   public String toString() {
-      return "Timeout";
-   }
-}
